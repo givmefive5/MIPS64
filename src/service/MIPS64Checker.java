bdd94bbc04433a968bdc9497af1d54088183245d
peigen831 <peigen831@yahoo.com>
2015-10-15 00:16:42 +0800

    pseodo parse, opcode for functions

--- src/service/MIPS64Checker.java
+++ src/service/MIPS64Checker.java
@@ -7,6 +7,12 @@ import exceptions.InvalidFormatException;
 public class MIPS64Checker {
 
 	public static List<String> parse(String text) throws InvalidFormatException {
+		//read each line start from .data
+		//parse the variables 	//variable name: .datatype size
+		
+		//until .text
+		//parse the command		//operation: reg/label
+		//generate instruction opcode
 		throw new InvalidFormatException("Parsing error, unknown format");
 	}
 }
